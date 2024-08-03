e612104a0c803ac56eee95768282c36fedccc2c6
mememe653 <effewe922@gmail.com>
2024-08-03 19:44:35 +0930

    Implement transaction receipt for shares module

--- financial_model/rust/financial_model/src/shares.rs
+++ financial_model/rust/financial_model/src/shares.rs
@@ -89,8 +89,48 @@ impl Params {
     }
 }
 
-//TODO:Implement Transaction
-pub struct Transaction {}
+struct BuyReceipt {
+    time: usize,
+    amount: f64,
+}
+
+impl BuyReceipt {
+    fn new(time: usize, amount: f64) -> Self {
+        Self {
+            time,
+            amount,
+        }
+    }
+}
+
+struct SellReceipt {
+    time: usize,
+    amount: f64,
+    taxable_amount: f64,
+}
+
+impl SellReceipt {
+    fn new(time: usize, paid_amount: f64, capital_gains: f64, weeks_held: usize) -> Self {
+        if weeks_held > 52 {
+            Self {
+                time,
+                amount: paid_amount + capital_gains,
+                taxable_amount: capital_gains / 2.0,
+            }
+        } else {
+            Self {
+                time,
+                amount: paid_amount + capital_gains,
+                taxable_amount: capital_gains,
+            }
+        }
+    }
+}
+
+pub enum Transaction {
+    Buy(BuyReceipt),
+    Sell(SellReceipt),
+}
 
 struct Share {
     initial_value: f64,
@@ -127,19 +167,19 @@ impl Asset {
         }
     }
 
-    pub fn simulate_timestep(&mut self, time: usize, params: Params, commands: &HashMap<usize, Vec<Command>>) -> Option<Transaction> {
+    pub fn simulate_timestep(&mut self, time: usize, params: Params, commands: &HashMap<usize, Vec<Command>>) -> Option<Vec<Transaction>> {
         self.shares.iter_mut()
                     .for_each(|share| share.simulate_timestep(&params));
         self.value[time] = self.shares.iter()
                                     .map(|share| share.initial_value + share.capital_gains)
                                     .sum();
-        //TODO:Implement receipt
-        let mut receipt = None;
+        let mut receipts = Vec::<Transaction>::new();
         if let Some(commands_vec) = commands.get(&time) {
             for command in commands_vec {
                 match command {
-                    Command::Buy(BuyCommand { amount, .. }) => {
+                    Command::Buy(BuyCommand { time, amount }) => {
                         self.shares.push_back(Share::new(*amount));
+                        receipts.push(Transaction::Buy(BuyReceipt::new(*time, *amount)));
                     },
                     Command::Sell(SellCommand { time, amount }) => {
                         let mut remaining_amount = amount.clone();
@@ -147,24 +187,42 @@ impl Asset {
                             let share_value = self.shares[0].initial_value +
                                                 self.shares[0].capital_gains;
                             if remaining_amount < share_value {
-                                self.shares[0].initial_value *= (share_value -
-                                                                 remaining_amount) /
-                                                                share_value;
-                                self.shares[0].capital_gains *= (share_value -
-                                                                 remaining_amount) /
-                                                                share_value;
+                                let paid_amount = remaining_amount *
+                                                    self.shares[0].initial_value /
+                                                    share_value;
+                                let cg_amount = remaining_amount *
+                                                self.shares[0].capital_gains /
+                                                share_value;
+                                let weeks_held = self.shares[0].weeks_held;
+                                receipts.push(Transaction::Sell(SellReceipt::new(*time,
+                                                                            paid_amount,
+                                                                            cg_amount,
+                                                                            weeks_held)));
+                                self.shares[0].initial_value -= paid_amount;
+                                self.shares[0].capital_gains -= cg_amount;
                                 remaining_amount = 0.0;
                             } else {
                                 remaining_amount -= self.shares[0].initial_value +
                                                     self.shares[0].capital_gains;
-                                self.shares.pop_front();
+                                let sold_share = self.shares.pop_front().unwrap();
+                                let paid_amount = sold_share.initial_value;
+                                let cg_amount = sold_share.capital_gains;
+                                let weeks_held = sold_share.weeks_held;
+                                receipts.push(Transaction::Sell(SellReceipt::new(*time,
+                                                                                 paid_amount,
+                                                                                 cg_amount,
+                                                                                 weeks_held)));
                             }
                         }
                     },
                 }
             }
         }
-        receipt
+        if receipts.len() > 0 {
+            Some(receipts)
+        } else {
+            None
+        }
     }
 
     pub fn write_to_file(&self, filepath: &str) {
