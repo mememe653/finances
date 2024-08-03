772f2ab8d574d46f9f366d593ee488ef960a0a9e
mememe653 <effewe922@gmail.com>
2024-08-03 15:20:57 +0930

    Add misc module

---
+++ financial_model/rust/financial_model/src/misc.rs
@@ -0,0 +1,18 @@
+use std::fs;
+
+//TODO:Have single source of truth for NUM_TIMESTEPS
+const NUM_TIMESTEPS: usize = 35 * 52;
+
+pub fn parse_input(file_path: &str) -> [f64; NUM_TIMESTEPS] {
+    let mut expenses = [0.0; NUM_TIMESTEPS];
+    let input_lines = fs::read_to_string(file_path)
+        .expect("Invalid file path");
+    for line in input_lines.lines() {
+        let fields: Vec<&str> = line.split_whitespace()
+                                    .collect();
+        let time: usize = fields[0].parse().unwrap();
+        let amount: f64 = fields[1].parse().unwrap();
+        expenses[time] += amount;
+    }
+    expenses
+}
