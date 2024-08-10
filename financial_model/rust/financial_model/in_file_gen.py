7102fdf599ab0e8b7c24114ce855f04cbd8beff7
mememe653 <effewe922@gmail.com>
2024-08-10 21:57:12 +0930

    Apply inflation when generating input files, except SG variant of superannuation

--- financial_model/rust/financial_model/in_file_gen.py
+++ financial_model/rust/financial_model/in_file_gen.py
@@ -1,7 +1,15 @@
+import math
+
+def apply_inflation(amount, time, annual_inflation_rate):
+    weekly_inflation_rate = (math.exp(math.log(1 + annual_inflation_rate / 100) / 52) - 1) * 100
+    return amount * (1 + weekly_inflation_rate / 100) ** time
+
+
 class Home:
-    def __init__(self, num_weeks):
+    def __init__(self, num_weeks, annual_inflation_rate):
         self.in_file = open("input_files/home.txt", "w")
         self.num_weeks = num_weeks
+        self.inflation_rate = annual_inflation_rate
         self.buy_list = {}
         self.sell_list = {}
 
@@ -18,7 +26,7 @@ class Home:
     def buy(self, amount, time):
         if time not in self.buy_list:
             self.buy_list[time] = []
-        self.buy_list[time].append(amount)
+        self.buy_list[time].append(apply_inflation(amount, time, self.inflation_rate))
 
     def sell(self, time):
         if time not in self.sell_list:
@@ -27,9 +35,10 @@ class Home:
 
 
 class HomeLoan:
-    def __init__(self, num_weeks):
+    def __init__(self, num_weeks, annual_inflation_rate):
         self.in_file = open("input_files/home_loan.txt", "w")
         self.num_weeks = num_weeks
+        self.inflation_rate = annual_inflation_rate
         self.buy_list = {}
         self.pay_list = {}
 
@@ -48,20 +57,21 @@ class HomeLoan:
         if time not in self.buy_list:
             self.buy_list[time] = []
         self.buy_list[time].append({
-            "amount": amount,
+            "amount": apply_inflation(amount, time, self.inflation_rate),
             "duration": duration,
         })
     
     def pay(self, amount, time):
         if time not in self.pay_list:
             self.pay_list[time] = []
-        self.pay_list[time].append(amount)
+        self.pay_list[time].append(apply_inflation(amount, time, self.inflation_rate))
 
 
 class CarLoan:
-    def __init__(self, num_weeks):
+    def __init__(self, num_weeks, annual_inflation_rate):
         self.in_file = open("input_files/car_loan.txt", "w")
         self.num_weeks = num_weeks
+        self.inflation_rate = annual_inflation_rate
         self.buy_list = {}
 
     def write(self):
@@ -77,16 +87,17 @@ class CarLoan:
         if time not in self.buy_list:
             self.buy_list[time] = []
         self.buy_list[time].append({
-            "amount": amount,
-            "balloon_payment": balloon_payment,
+            "amount": apply_inflation(amount, time, self.inflation_rate),
+            "balloon_payment": apply_inflation(balloon_payment, time, self.inflation_rate),
             "duration": duration,
         })
 
 
 class Hecs:
-    def __init__(self, num_weeks):
+    def __init__(self, num_weeks, annual_inflation_rate):
         self.in_file = open("input_files/hecs.txt", "w")
         self.num_weeks = num_weeks
+        self.inflation_rate = annual_inflation_rate
         self.pay_list = {}
 
     def write(self):
@@ -100,14 +111,15 @@ class Hecs:
         if time not in self.pay_list:
             self.pay_list[time] = []
         self.pay_list[time].append({
-            "amount": amount,
+            "amount": apply_inflation(amount, time, self.inflation_rate),
         })
 
 
 class Shares:
-    def __init__(self, num_weeks):
+    def __init__(self, num_weeks, annual_inflation_rate):
         self.in_file = open("input_files/shares.txt", "w")
         self.num_weeks = num_weeks
+        self.inflation_rate = annual_inflation_rate
         self.buy_list = {}
         self.sell_list = {}
 
@@ -125,21 +137,22 @@ class Shares:
         if time not in self.buy_list:
             self.buy_list[time] = []
         self.buy_list[time].append({
-            "amount": amount,
+            "amount": apply_inflation(amount, time, self.inflation_rate),
         })
 
     def sell(self, amount, time):
         if time not in self.sell_list:
             self.sell_list[time] = []
         self.sell_list[time].append({
-            "amount": amount,
+            "amount": apply_inflation(amount, time, self.inflation_rate),
         })
 
 
 class Superannuation:
-    def __init__(self, num_weeks):
+    def __init__(self, num_weeks, annual_inflation_rate):
         self.in_file = open("input_files/super.txt", "w")
         self.num_weeks = num_weeks
+        self.inflation_rate = annual_inflation_rate
         self.buy_sg_list = {}
         self.buy_cc_list = {}
         self.buy_ncc_list = {}
@@ -163,6 +176,7 @@ class Superannuation:
 
     def buy(self, amount, variant, time):
         if variant == "SG":
+            #TODO:Handle inflation in SG variant
             if time not in self.buy_sg_list:
                 self.buy_sg_list[time] = []
             self.buy_sg_list[time].append({
@@ -172,27 +186,28 @@ class Superannuation:
             if time not in self.buy_cc_list:
                 self.buy_cc_list[time] = []
             self.buy_cc_list[time].append({
-                "amount": amount,
+                "amount": apply_inflation(amount, time, self.inflation_rate),
             })
         elif variant == "NCC":
             if time not in self.buy_ncc_list:
                 self.buy_ncc_list[time] = []
             self.buy_ncc_list[time].append({
-                "amount": amount,
+                "amount": apply_inflation(amount, time, self.inflation_rate),
             })
 
     def sell(self, amount, time):
         if time not in self.sell_list:
             self.sell_list[time] = []
         self.sell_list[time].append({
-            "amount": amount,
+            "amount": apply_inflation(amount, time, self.inflation_rate),
         })
 
 
 class Income:
-    def __init__(self, num_weeks):
+    def __init__(self, num_weeks, annual_inflation_rate):
         self.in_file = open("input_files/income.txt", "w")
         self.num_weeks = num_weeks
+        self.inflation_rate = annual_inflation_rate
         self.income_list = {}
 
     def write(self):
@@ -206,14 +221,15 @@ class Income:
         if time not in self.income_list:
             self.income_list[time] = []
         self.income_list[time].append({
-            "amount": amount,
+            "amount": apply_inflation(amount, time, self.inflation_rate),
         })
 
 
 class Misc:
-    def __init__(self, num_weeks):
+    def __init__(self, num_weeks, annual_inflation_rate):
         self.in_file = open("input_files/misc.txt", "w")
         self.num_weeks = num_weeks
+        self.inflation_rate = annual_inflation_rate
         self.buy_list = {}
 
     def write(self):
@@ -227,21 +243,22 @@ class Misc:
         if time not in self.buy_list:
             self.buy_list[time] = []
         self.buy_list[time].append({
-            "amount": amount,
+            "amount": apply_inflation(amount, time, self.inflation_rate),
         })
 
 
 if __name__ == "__main__":
     num_weeks = 35 * 52
+    inflation_rate = 4
 
-    income_file_gen = Income(num_weeks)
-    misc_file_gen = Misc(num_weeks)
-    home_file_gen = Home(num_weeks)
-    home_loan_file_gen = HomeLoan(num_weeks)
-    car_loan_file_gen = CarLoan(num_weeks)
-    shares_file_gen = Shares(num_weeks)
-    superannuation_file_gen = Superannuation(num_weeks)
-    hecs_file_gen = Hecs(num_weeks)
+    income_file_gen = Income(num_weeks, inflation_rate)
+    misc_file_gen = Misc(num_weeks, inflation_rate)
+    home_file_gen = Home(num_weeks, inflation_rate)
+    home_loan_file_gen = HomeLoan(num_weeks, inflation_rate)
+    car_loan_file_gen = CarLoan(num_weeks, inflation_rate)
+    shares_file_gen = Shares(num_weeks, inflation_rate)
+    superannuation_file_gen = Superannuation(num_weeks, inflation_rate)
+    hecs_file_gen = Hecs(num_weeks, inflation_rate)
 
     for week in range(num_weeks):
         income_file_gen.add(1442, week)
